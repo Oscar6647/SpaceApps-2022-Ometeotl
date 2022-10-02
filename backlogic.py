57e4c632727422bff36ef6998b90aa2d871d6610
Oscar6647 <A01284083@itesm.mx>
2022-10-02 10:32:46 -0500

    TEST#2 (NO FLUTTER YET)

--- backlogic.py
+++ backlogic.py
@@ -1,12 +1,12 @@
 #THIS FILE IS A TEST (NOT FINAL VERSION)
 
-from flask import Flask 
+from flask import Flask,render_template 
 
 app= Flask (__name__)
 
 @app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 if __name__ == 'main':
     app.run()
